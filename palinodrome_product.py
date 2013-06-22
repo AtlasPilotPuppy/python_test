f8c07bc46008b1642a96ead23936dfca0cd8884d
Justin Findlay <jfindlay@gmail.com>
2013-06-22 10:16:17 -0600

    ordering of palindrome product space

--- palinodrome_product.py
+++ palinodrome_product.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python
+
 """A palindromic number reads the same both ways. The largest palindrome made
 from the product of two 2-digit numbers is 9009 = 91 99.Find the largest
 palindrome made from the product of two 3-digit numbers."""
+
+# an unadorned 'digit' term implies base-ten
+
+# for a positive integer, n, the ordering of the products of any two integers
+# between n and 0 can be written:
+# n*n > n*(n - 1) > (n - 1)*(n - 1) > ... > (n - i)*(n - i - 1) > (n - i - 1)*(n - i - 1) > ... > 1*1 > 1*0 = 0*0
+
+def find():
+
+
+if __name__ == '__main__' : find()
