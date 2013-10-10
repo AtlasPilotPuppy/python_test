4acd10b863276cd815070f14ca978096f40931b7
Lucas David-Roesler <lucas.a.david-roesler@saic.com>
2013-10-10 12:40:26 -0700

    Update palindrome solution and add series product solution.

--- palinodrome_product.py
+++ palinodrome_product.py
@@ -5,35 +5,129 @@ palindrome made from the product of two 3-digit numbers."""
 
 
 def isPalindrome(number):
-        number = str(number)
-        reversed = number[::-1]
-        if number==reversed:
-                return True
-        else:
-                return False
-# brute force search
+    """
+    Test if the given number is a palindrome.
+
+    Returns a boolean.
+    """
+    number = str(number)
+    reversed = number[::-1]
+    if number==reversed:
+        return True
+    else:
+        return False
+
+def brute_force():
+    """
+    Perform a brute force search for the largest palindrome made from the
+    product of two 3-digit numbers.
+
+    Returns a tuple consisting of the palindrom and its 3-digit factors.
+
+    Example:
+    >>> brute_force()
+    (906609,913,993)
+
+    """
+    # parmeters ( we want to search through products of 3-digit numbers )
+    floor = 100
+    ceiling = 999
+
+    # initialize variables
+    factor1 = 0
+    factor2 = 0
+    product = 0
+    highestPalindrome = 0
+
+    # I excpect this search can be reduced by finding appropriate factors
+    # or some other number theory math idea about palindromes.
+    for i in range(floor,ceiling+1):
+            for j in range(floor,ceiling+1):
+                    product = i * j
+                    if (isPalindrome(product) and (highestPalindrome < product) ):
+                        highestPalindrome = product
+                        factor1 = i
+                        factor2 = j
+    return highestPalindrome, factor1, factor2
 
 
-# parmeters ( we want to search through products of 3-digit numbers )
-floor = 100
-ceiling = 999
+def brute_force_no_doubles():
+    """
+    Perform a brute force search for the largest palindrome made from the
+    product of two 3-digit numbers.
 
-# initialize variables
-factor1 = 0
-factor2 = 0
-product = 0
-highestPalindrome = 0
+    Returns a tuple consisting of the palindrom and its 3-digit factors.
 
-# I excpect this search can be reduced by finding appropriate factors
-# or some other number theory math idea about palindromes.
-for i in range(floor,ceiling+1):
-        for j in range(floor,ceiling+1):
-                product = i * j
-                if (isPalindrome(product) and (highestPalindrome < product) ):
-                    highestPalindrome = product
-                    factor1 = i
-                    factor2 = j
+    This method eliminates double checking pairs, e.g. brute_force() checks
+    both i=500, j=900 and i=900, j=500.  To eliminate this we can assume that
+    we always have j>=i.
+
+    Example:
+    >>> brute_force()
+    (906609,913,993)
+
+    """
+    # parmeters ( we want to search through products of 3-digit numbers )
+    floor = 100
+    ceiling = 999
+
+    # initialize variables
+    factor1 = 0
+    factor2 = 0
+    product = 0
+    highestPalindrome = 0
+
+    # I excpect this search can be reduced by finding appropriate factors
+    # or some other number theory math idea about palindromes.
+    for i in range(floor,ceiling+1):
+            for j in range(i,ceiling+1):
+                    product = i * j
+                    if (isPalindrome(product) and (highestPalindrome < product) ):
+                        highestPalindrome = product
+                        factor1 = i
+                        factor2 = j
+    return highestPalindrome, factor1, factor2
 
 
-print "Largest palindrome made from the product of two 3-digit numbers is \
+def brute_force_count_down():
+    """
+    Perform a brute force search for the largest palindrome made from the
+    product of two 3-digit numbers.
+
+    Returns a tuple consisting of the palindrom and its 3-digit factors.
+
+    If we count down from 999 rather than up to 999, once we run into a palindrome
+    there is no need to check smaller factors in that loop, so we can save time
+    not checking those pairs.
+
+    Example:
+    >>> brute_force()
+    (906609,913,993)
+
+    """
+    # parmeters ( we want to search through products of 3-digit numbers )
+    floor = 100
+    ceiling = 999
+
+    # initialize variables
+    factor1 = 0
+    factor2 = 0
+    product = 0
+    highestPalindrome = 0
+
+    for i in range(ceiling,floor-1,-1):
+        for j in range(ceiling,i-1,-1):
+            product = i*j
+            factor1 = i
+            factor2 = j
+            if product <= highestPalindrome:
+                break # no need to check smaller j, from this point on i*j will always be too small. 
+            if isPalindrome(product):
+                highestPalindrome = product
+
+    return highestPalindrome, factor1, factor2
+
+if __name__ == "__main__":
+    highestPalindrome, factor1, factor2 = brute_force_count_down()
+    print "Largest palindrome made from the product of two 3-digit numbers is \
 {:,} made from {} * {}.".format(highestPalindrome, factor1, factor2)
--- series_product.py
+++ series_product.py
@@ -1,3 +1,77 @@
 """Complete this program to find the greatest produdct of five consecutive
 digits in the list"""
-in_file = open('array.txt')
+
+def string_product(string):
+    """
+    Return the product of the integers in string.
+
+    Argument:
+        string -- a string containing only integers.
+
+    Example:
+
+    >>>  string_product('123')
+    6
+    
+    """
+    product = 1
+
+    for i in range(len(string)):
+        product *= int(string[i])
+
+    return product
+
+
+
+def max_consecutive_product(string, length):
+    """
+    Iterate over string and find the largest product of a substring with the
+    given length.
+
+    Arguments:
+        string -- string containing only integers.
+        length -- size of substring to consider.
+
+    Example:
+    >>> max_consecutive_product('12345', 2)
+    20
+
+    """
+    max_product = 0
+
+    for i in range(len(string)-length+1):
+        sub = string[i:i+length]
+        product = string_product(sub)
+        if product > max_product:
+            max_product = product
+
+    return max_product
+
+if __name__ == "__main__":
+    max_product = 0
+
+    # open the file
+    with open('array.txt') as in_file:
+        # Concatenating the current line with the last 4 characters of the 
+        # previous line allows us to consider the product of 5  consecutive 
+        # characters spanning those two lines.
+        prev_line = ''  # initial empty line
+        for line in in_file:
+            # needle will be the string we actually iterate through to find
+            # the max product. 
+            line = line.strip()
+            needle = prev_line + line
+
+            # save the last 4 characters of the line for the next stage
+            if len(line)>4:
+                prev_line = line[-4:]
+            else:
+                prev_line = line
+
+            # look for the max
+            product = max_consecutive_product(needle, 5)
+            if product > max_product:
+                max_product = product
+
+    print "The greatest product of 5 consecutive digits in the list is\
+    {}".format(max_product)
