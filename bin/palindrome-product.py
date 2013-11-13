7a11b3761f3213153c0b9e1fc21fbf20c66fdb44
Jonathan Randall <jonathar@gmail.com>
2013-11-13 02:29:49 -0700

    Added solution for palindrome

---
+++ bin/palindrome-product.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import sys
+import datetime
+from datetime import timedelta
+from palindrome.products import largest_palindrome
+
+desc = '''Find the largest palindrome made from the\
+product of two 3-digit numbers.'''
+
+start = datetime.datetime.now()
+result = largest_palindrome(3)
+finish = datetime.datetime.now()
+
+msg = '''That largest palindrome with two 3-digit factors is {0:,}
+It's factors are {1} & {2}
+Finished in {3}.{4} seconds'''
+
+delta = finish - start
+print msg.format(result.palindrome,
+                 result.factor1,
+                 result.factor2,
+                 delta.seconds,
+                 delta.microseconds)
