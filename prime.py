4da6d8688b9ea509397bfcdc72d6817c93c6cfdb
ericsaupe <ericsaupe@gmail.com>
2013-12-03 20:24:23 -0800

    Completed Python tests.

--- prime.py
+++ prime.py
@@ -1,2 +1,32 @@
+#!/bin/env python2.7
+
 """Write a program to find the 10 001st prime number using the sieve of
 aristothanes."""
+
+"""
+Algorithm for Sieve of Eratosthenes modified to stop when the 10,001st prime
+has been found.
+
+Author:
+    Eric Saupe
+"""
+
+#Initialize variables.
+#We set the list to be large to ensure we get 10001 prime numbers
+numbers = [True] * 250000
+#0 and 1 should not be used in the calculation of primes
+numbers[0] = False
+numbers[1] = False
+#Keep track of the primes we've found so we can stop early
+primes = []
+
+for (i, prime) in enumerate(numbers):
+    #If prime, save it and mark all multiples of it as not prime
+    if prime:
+        primes.append(i)
+        for j in range(i * i, 250000, i):
+            numbers[j] = False
+    #If we have reached our limit, stop
+    if len(primes) == 10001:
+        print '%s is the 10,001st prime number' % primes[-1]
+        break
--- series_product.py
+++ series_product.py
@@ -1,3 +1,40 @@
-"""Complete this program to find the greatest produdct of five consecutive
+#!/bin/env python2.7
+
+from copy import copy
+
+"""Complete this program to find the greatest product of five consecutive
 digits in the list"""
+
+"""
+Loop to find the greatest product of five consectuive digits read from a file.
+Assumes that line breaks do not count as separation of digits, meaning it
+reads line breaks, or any non-integer character, as if it weren't there.
+
+Author:
+    Eric Saupe
+"""
 in_file = open('array.txt')
+
+#Initialize variables
+greatest_product = 0
+greatest_numbers = []
+numbers = []
+
+for c in in_file.read():
+    #If we don't have 5 numbers add the character if it's an int
+    if len(numbers) < 5:
+        try:
+            numbers.append(int(c))
+        except:
+            pass
+    #If we have 5 numbers, multiply and check, pop first number
+    if len(numbers) == 5:
+        product = reduce(lambda x, y: x * y, numbers)
+        if product > greatest_product:
+            greatest_product = product
+            #Make a copy so values aren't changed
+            greatest_numbers = copy(numbers)
+        numbers.pop(0)
+
+#Output result
+print '%s = %s' % (greatest_product, greatest_numbers)
